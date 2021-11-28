dedc0a0e476b94e136d6547a5901f6886325b861
Devin Adams <adams.devin.c@gmail.com>
2021-11-27 21:46:32 -0800

    I figured out list comprehension I think

--- main.py
+++ main.py
@@ -1,73 +1,75 @@
 import json
 import pandas as pd
-from time import sleep
 
 
-def itemlookup(items):
-
-    armor = {}
-    ancestry = {}
-    ability_score = {}
-    feats_class = {}
-    deity = {}
-
-    for i in items.index:
-        match i[:2]:
-            case 'Pe':
-                print(items[i]['name'])
-            case 'sp':
-                print(items[i]['name'])
-            case 'ab':
-                print(items[i]['name'])
-            case 'as':
-                ability_score = {items[i]['name']: items[i]}
-                del ability_score[str(items[i]['name'])]['name']
-                print(ability_score)
-            case 'sk':
-                print(items[i]['name'])
-            case 'sv':
-                print(items[i]['name'])
-            case 'ac':
-                print(items[i]['name'])
-            case 'an':
-                ancestry = {items[i]['name']: items[i]}
-                del ancestry[str(items[i]['name'])]['name']
-            case 'ar':
-                armor = {items[i]['name']: items[i]}
-                del armor[str(items[i]['name'])]['name']
-            case 'cl':
-                feats_class = {items[i]['name']: items[i]}
-                del feats_class[str(items[i]['name'])]['name']
-            case 'de':
-                deity = {items[i]['name']: items[i]}
-                del deity[str(items[i]['name'])]['name']
-            case 'ft':
-                print(items[i]['name'])
-            case 'fs':
-                print(items[i]['name'])
-            case 'wp':
-                print(items[i]['name'])
-            case 'gr':
-                print(items[i]['name'])
-            case 'sf':
-                print(items[i]['name'])
-            case 'ln':
-                print(items[i]['name'])
-            case 'hr':
-                print(items[i]['name'])
-            case 'rs':
-                print(items[i]['name'])
-            case _:
-                print(i)
-                pass
-    return ancestry, armor, ability_score, feats_class, deity
-
-# def itemParse(name, item_type):
-# print(str(name)+' '+str(item_type))
-# pass
+def initActor():
+    exp_file = open('token.json', encoding="utf8")
+    token = json.load(exp_file)
+    return token['actors']['actor.1']
 
 
-exp_file = open('token.json', encoding="utf8")
-token = json.load(exp_file)
-tokenItems = pd.Series(token['actors']['actor.1']['items'])
+items = pd.Series(initActor()['items'])
+name = initActor()['name']
+print(pd.Series(initActor())['gameValues'])
+#alignment = initActor()['alignment']
 
+
+def itemtodict(key):
+    itemtype = {items[key]['name']: items[key]}
+    del itemtype[str(items[key]['name'])]['name']
+    return itemtype
+
+
+for i in items.index:
+    match i[:2]:
+        case 'Pe':
+            perception = i
+        case 'sp':
+            spells = itemtodict(i)
+        case 'ab':
+            abilities = itemtodict(i)
+        case 'as':
+            ability_score = itemtodict(i)
+        case 'sk':
+            skills = itemtodict(i)
+        case 'sv':
+            saves = itemtodict(i)
+        case 'ac':
+            armor_class = i
+        case 'an':
+            ancestry = itemtodict(i)
+        case 'ar':
+            armor = itemtodict(i)
+        case 'cl':
+            classFeats = itemtodict(i)
+        case 'de':
+            deity = itemtodict(i)
+        case 'ft':
+            generalFeats = itemtodict(i)
+        case 'fs':
+            skillFeats = itemtodict(i)
+        case 'wp':
+            weapons = itemtodict(i)
+        case 'gr':
+            gear = itemtodict(i)
+        case 'sf':
+            staves = itemtodict(i)
+        case 'ln':
+            languages = itemtodict(i)
+        case 'hr':
+            heritage = itemtodict(i)
+        case 'rv':
+            match i[:4]:
+                case 'rvFo':
+                    focusPoints = i
+                case 'rvHe':
+                    heroPoints = i
+                case _:
+                    healthPoints = i
+        case 'mv':
+            movespeed = i
+        case 'nw':
+            fists = i
+        case _:
+            print(i)
+            pass
