3e9689d96edc87cfc26b950c51c34aff5cca4ef3
Devin Adams <adams.devin.c@gmail.com>
2021-11-28 18:55:55 -0800

    NOW I've figured out list comprehension

--- main.py
+++ main.py
@@ -1,75 +1,42 @@
 import json
-import pandas as pd
 
 
 def initActor():
     exp_file = open('token.json', encoding="utf8")
     token = json.load(exp_file)
-    return token['actors']['actor.1']
+    return token['actors']['actor.1']['items']
 
 
-items = pd.Series(initActor()['items'])
-name = initActor()['name']
-print(pd.Series(initActor())['gameValues'])
-#alignment = initActor()['alignment']
+def delName(itemdict):
+    dictname = [key for key in itemdict]
+    for key in dictname:
+        del itemdict[key]['name']
+    return itemdict
 
 
-def itemtodict(key):
-    itemtype = {items[key]['name']: items[key]}
-    del itemtype[str(items[key]['name'])]['name']
-    return itemtype
+items = initActor()
 
-
-for i in items.index:
-    match i[:2]:
-        case 'Pe':
-            perception = i
-        case 'sp':
-            spells = itemtodict(i)
-        case 'ab':
-            abilities = itemtodict(i)
-        case 'as':
-            ability_score = itemtodict(i)
-        case 'sk':
-            skills = itemtodict(i)
-        case 'sv':
-            saves = itemtodict(i)
-        case 'ac':
-            armor_class = i
-        case 'an':
-            ancestry = itemtodict(i)
-        case 'ar':
-            armor = itemtodict(i)
-        case 'cl':
-            classFeats = itemtodict(i)
-        case 'de':
-            deity = itemtodict(i)
-        case 'ft':
-            generalFeats = itemtodict(i)
-        case 'fs':
-            skillFeats = itemtodict(i)
-        case 'wp':
-            weapons = itemtodict(i)
-        case 'gr':
-            gear = itemtodict(i)
-        case 'sf':
-            staves = itemtodict(i)
-        case 'ln':
-            languages = itemtodict(i)
-        case 'hr':
-            heritage = itemtodict(i)
-        case 'rv':
-            match i[:4]:
-                case 'rvFo':
-                    focusPoints = i
-                case 'rvHe':
-                    heroPoints = i
-                case _:
-                    healthPoints = i
-        case 'mv':
-            movespeed = i
-        case 'nw':
-            fists = i
-        case _:
-            print(i)
-            pass
+rv = {k: v for (k, v) in items.items() if k[:2] == 'rv'}
+perception = {v['name']: v for (k, v) in items.items() if k[:2] == 'Pe'}
+spells = delName({v['name']: v for (k, v) in items.items() if k[:2] == 'sp'})
+abilities = delName({v['name']: v for (k, v) in items.items() if k[:2] == 'ab'})
+abilityScores = delName({v['name']: v for (k, v) in items.items() if k[:2] == 'as'})
+skills = delName({v['name']: v for (k, v) in items.items() if k[:2] == 'sl'})
+saves = delName({v['name']: v for (k, v) in items.items() if k[:2] == 'sv'})
+armorClass = delName({k: v for (k, v) in items.items() if k[:2] == 'ac'})
+ancestry = delName({v['name']: v for (k, v) in items.items() if k[:2] == 'an'})
+armor = delName({v['name']: v for (k, v) in items.items() if k[:2] == 'ar'})
+classFeats = delName({v['name']: v for (k, v) in items.items() if k[:2] == 'cl'})
+deity = delName({v['name']: v for (k, v) in items.items() if k[:2] == 'de'})
+generalFeats = delName({v['name']: v for (k, v) in items.items() if k[:2] == 'ft'})
+skillFeats = delName({v['name']: v for (k, v) in items.items() if k[:2] == 'fs'})
+weapons = delName({v['name']: v for (k, v) in items.items() if k[:2] == 'wp'})
+gear = delName({v['name']: v for (k, v) in items.items() if k[:2] == 'gr'})
+staves = delName({v['name']: v for (k, v) in items.items() if k[:2] == 'sf'})
+languages = delName({v['name']: v for (k, v) in items.items() if k[:2] == 'ln'})
+heritage = delName({v['name']: v for (k, v) in items.items() if k[:2] == 'hr'})
+movement = delName({v['name']: v for (k, v) in items.items() if k[:2] == 'mv'})
+fists = delName({v['name']: v for (k, v) in items.items() if k[:2] == 'nw'})
+focusPoints = delName({v['name']: v for (k, v) in rv.items() if k[:4] == 'rvFo'})
+heroPoints = delName({v['name']: v for (k, v) in rv.items() if k[:4] == 'rvHe'})
+hitPoints = delName({v['name']: v for (k, v) in rv.items() if k[:4] == 'rvHi'})
