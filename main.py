09f2800b755b14a0b3fce9d4f76f5f5b34efc96b
adams <adams.devin.c@gmail.com>
2021-12-04 11:46:34 -0800

    removed my attempt at a tkinter gui, need to rethink that

--- main.py
+++ main.py
@@ -1,34 +1,34 @@
 import json
-from tkinter import Tk, filedialog, StringVar
-from pathlib import Path
-import os
 
 
 def init_actor():
-    jpath = filedialog.askopenfilename(initialdir=str(Path.home()))
-    token = json.load(os.path.basename(jpath))
+    jpath = open('token.json', encoding="utf8")
+    token = json.load(jpath)
     tokenactor = token['actors']['actor.1']
-    tokenitems = actor['items']
+    tokenitems = tokenactor['items']
     return tokenactor, tokenitems
 
 
 def del_name(itemdict):
     dictname = [key for key in itemdict]
     for key in dictname:
-        del itemdict[key]['name']
+        if key == 'ArmorClass':
+            del itemdict[key]['compset']
+        else:
+            del itemdict[key]['name']
     return itemdict
 
 
 actor, items = init_actor()
 
-rv = {k: v for (k, v) in items.items() if k[:2] == 'rv'}
-perception = {v['name']: v for (k, v) in items.items() if k[:2] == 'Pe'}
+
+perception = del_name({v['name']: v for (k, v) in items.items() if k[:2] == 'Pe'})
 spells = del_name({v['name']: v for (k, v) in items.items() if k[:2] == 'sp'})
 abilities = del_name({v['name']: v for (k, v) in items.items() if k[:2] == 'ab'})
 abilityScores = del_name({v['name']: v for (k, v) in items.items() if k[:2] == 'as'})
 skills = del_name({v['name']: v for (k, v) in items.items() if k[:2] == 'sl'})
 saves = del_name({v['name']: v for (k, v) in items.items() if k[:2] == 'sv'})
-armorClass = del_name({k: v for (k, v) in items.items() if k[:2] == 'ac'})
+armorClass = del_name({v['compset']: v for (k, v) in items.items() if k[:2] == 'ac'})
 ancestry = del_name({v['name']: v for (k, v) in items.items() if k[:2] == 'an'})
 armor = del_name({v['name']: v for (k, v) in items.items() if k[:2] == 'ar'})
 classFeats = del_name({v['name']: v for (k, v) in items.items() if k[:2] == 'cl'})
@@ -42,6 +42,6 @@ languages = del_name({v['name']: v for (k, v) in items.items() if k[:2] == 'ln'}
 heritage = del_name({v['name']: v for (k, v) in items.items() if k[:2] == 'hr'})
 movement = del_name({v['name']: v for (k, v) in items.items() if k[:2] == 'mv'})
 fists = del_name({v['name']: v for (k, v) in items.items() if k[:2] == 'nw'})
-focusPoints = del_name({v['name']: v for (k, v) in rv.items() if k[:4] == 'rvFo'})
-heroPoints = del_name({v['name']: v for (k, v) in rv.items() if k[:4] == 'rvHe'})
-hitPoints = del_name({v['name']: v for (k, v) in rv.items() if k[:4] == 'rvHi'})
+focusPoints = del_name({v['name']: v for (k, v) in items.items() if k[:5] == 'rvFoc'})
+heroPoints = del_name({v['name']: v for (k, v) in items.items() if k[:5] == 'rvHer'})
+hitPoints = del_name({v['name']: v for (k, v) in items.items() if k[:5] == 'rvHit'})
